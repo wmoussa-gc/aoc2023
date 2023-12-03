8ff7af674a926284c8277b6cc8cfbff9dda70ccc
Wissam Moussa <wissam.moussa@cds-snc.ca>
2023-12-03 10:43:57 -0500

    day 3

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         part1();
-        part2();
+        part2_alter();
     }
 
     private static void part1() throws IOException {
@@ -53,6 +55,31 @@ public class Main {
             total += temp;
         }
         System.out.println(total);
+    }
+
+    /**
+     * credit to <a href="https://github.com/vuryss/aoc-java/blob/master/src/main/java/com/vuryss/aoc/solutions/event2023/Day1.java">...</a>
+     * @throws IOException
+     */
+    private static void part2_alter() throws IOException {
+        var digitWords = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        String fileName = "src/day1/input.txt";
+        List<String> lines = Files.readAllLines(Path.of(fileName));
+        long sum = 0L;
+
+        for (var line: lines) {
+            var numbers = new LinkedList<Integer>();
+
+            for (int i = 0; i < line.length(); i++) {
+                int index = i;
+                Stream.of(line.charAt(i)).filter(Character::isDigit).forEach(c -> numbers.add(Character.getNumericValue(c)));
+                digitWords.stream().filter(e -> line.startsWith(e, index)).forEach(e -> numbers.add(digitWords.indexOf(e) + 1));
+            }
+
+            sum += numbers.getFirst() * 10 + numbers.getLast();
+        }
+
+        System.out.println(sum);
     }
 
     private static boolean isNumeric(String str) {
--- src/day3/Main.java
+++ src/day3/Main.java
@@ -1,48 +1,220 @@
 package day3;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.Character.isDigit;
+import static java.nio.file.Files.readAllLines;
 
 public class Main {
+    private final static String FILENAME = "src/" + Main.class.getPackageName() + "/input.txt";
+
     public static void main(String[] args) throws IOException {
-        part1();
-        //part2();
+        //part1();
+        part2();
     }
 
     private static void part1() throws IOException {
-        String fileName = "src/day3/sample.txt";
-        List<String> lines = Files.readAllLines(Path.of(fileName));
-
+        List<String> lines = readAllLines(Path.of(FILENAME));
+        int number_of_rows = lines.size();
+        int number_of_columns = lines.get(0).length();
+        char[][] matrix = new char[number_of_rows][number_of_columns];
+        int i = 0;
         for (String line : lines) {
+            matrix[i++] = line.toCharArray();
+        }
+
+        long sum = 0;
+        for (int row = 0; row < number_of_rows; row++) {
+            boolean is_surrounded = false;
+            String part = "";
+            for (int col = 0; col < number_of_columns; col++) {
+                if (isDigit(matrix[row][col])) {
+                    part += matrix[row][col];
+                    if (isSurrounded(matrix, row, col, number_of_rows))
+                        is_surrounded = true;
+                } else if (!part.isEmpty() && is_surrounded) {
+                    System.out.println(part);
+                    sum += Integer.parseInt(part);
+                    part = ""; //reset
+                    is_surrounded = false;
+                } else if (!part.isEmpty()) {
+                    part = "";
+                }
+            }
+
+            // last col
+            if (!part.isEmpty() && is_surrounded) {
+                System.out.println(part);
+                sum += Integer.parseInt(part);
+            }
 
         }
+        System.out.println("total= " + sum);
+    }
+
+    private static boolean isSurrounded(char[][] matrix, int row, int col, int size) {
+        if (row != 0 && col != 0 && row != size - 1 && col != size - 1) {
+            if (!isDigit(matrix[row + 1][col + 1]) && matrix[row + 1][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col - 1]) && matrix[row - 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col + 1]) && matrix[row - 1][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col - 1]) && matrix[row + 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.') return true;
+            else return !isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.';
+        } else if (row == 0 && col == 0) {
+            if (!isDigit(matrix[row + 1][col + 1]) && matrix[row + 1][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else return !isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.';
+        } else if (row == 0 && col == size - 1) {
+            if (!isDigit(matrix[row + 1][col - 1]) && matrix[row + 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else return !isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.';
+        } else if (row == size - 1 && col == 0) {
+            if (!isDigit(matrix[row - 1][col + 1]) && matrix[row - 1][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else return !isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.';
+        } else if (row == size - 1 && col == size - 1) {
+            if (!isDigit(matrix[row - 1][col - 1]) && matrix[row - 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else return !isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.';
+        } else if (row == 0 && col != size - 1) {
+            if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col + 1]) && matrix[row + 1][col + 1] != '.') return true;
+            else return !isDigit(matrix[row + 1][col - 1]) && matrix[row + 1][col - 1] != '.';
+        } else if (col == 0) {
+            if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col + 1]) && matrix[row + 1][col + 1] != '.') return true;
+            else return !isDigit(matrix[row - 1][col + 1]) && matrix[row - 1][col + 1] != '.';
+        } else if (row == size - 1) {
+            if (!isDigit(matrix[row][col + 1]) && matrix[row][col + 1] != '.') return true;
+            else if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col + 1]) && matrix[row - 1][col + 1] != '.') return true;
+            else return !isDigit(matrix[row - 1][col - 1]) && matrix[row - 1][col - 1] != '.';
+        } else if (col == size - 1 && row != 0) {
+            if (!isDigit(matrix[row][col - 1]) && matrix[row][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row + 1][col - 1]) && matrix[row + 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col - 1]) && matrix[row - 1][col - 1] != '.') return true;
+            else if (!isDigit(matrix[row - 1][col]) && matrix[row - 1][col] != '.') return true;
+            else return !isDigit(matrix[row + 1][col]) && matrix[row + 1][col] != '.';
+        }
+
+
+        return false;
     }
 
     private static void part2() throws IOException {
-        String fileName = "src/day2/input.txt";
-        List<String> lines = Files.readAllLines(Path.of(fileName));
-        int sum = 0;
+        List<String> lines = readAllLines(Path.of(FILENAME));
+        int number_of_rows = lines.size();
+        int number_of_columns = lines.get(0).length();
+        char[][] matrix = new char[number_of_rows][number_of_columns];
+        int i = 0;
         for (String line : lines) {
-            String[] reveals = line.split(":")[1].split(";");
-            int min_green = 0;
-            int min_red = 0;
-            int min_blue = 0;
-            for (String reveal : reveals) {
-                String[] balls = reveal.split(",");
-                for (String ball : balls) {
-                    String[] temp = ball.trim().split(" ");
-                    int x = Integer.parseInt(temp[0]);
-                    String color = temp[1];
-                    if (color.equals("red") && x > min_red) min_red = x;
-                    if (color.equals("green") && x > min_green) min_green = x;
-                    if (color.equals("blue") && x > min_blue) min_blue = x;
+            matrix[i++] = line.toCharArray();
+        }
+        Map<String, List<Integer>> gears_and_part = new HashMap<>();
+        for (int row = 0; row < number_of_rows; row++) {
+            String is_surrounded_by_gear = null;
+            String part = "";
+            for (int col = 0; col < number_of_columns; col++) {
+                if (isDigit(matrix[row][col])) {
+                    part += matrix[row][col];
+                    is_surrounded_by_gear = is_surrounded_by_gear != null ? is_surrounded_by_gear : isSurroundedByGear(matrix, row, col, number_of_rows);
+                } else if (!part.isEmpty() && is_surrounded_by_gear != null) {
+                    System.out.println(part);
+                    List<Integer> parts = gears_and_part.getOrDefault(is_surrounded_by_gear, new ArrayList<>());
+                    parts.add(Integer.parseInt(part));
+                    gears_and_part.put(is_surrounded_by_gear, parts);
+                    part = ""; //reset
+                    is_surrounded_by_gear = null;
+                } else if (!part.isEmpty()) {
+                    part = "";
                 }
             }
-            sum += min_green * min_blue * min_red;
+
+            // last col
+            if (!part.isEmpty() && is_surrounded_by_gear != null) {
+                System.out.println(part);
+                List<Integer> parts = gears_and_part.getOrDefault(is_surrounded_by_gear, new ArrayList<>());
+                parts.add(Integer.parseInt(part));
+                gears_and_part.put(is_surrounded_by_gear, parts);
+            }
+
         }
-        System.out.println(sum);
+
+        long sum = 0;
+        for (List<Integer> s : gears_and_part.values()) {
+            if (s.size() == 2) sum += s.get(0) * s.get(1);
+        }
+        System.out.println("total gears= " + gears_and_part);
+        System.out.println("sum= " + sum);
+
     }
+
+    private static String isSurroundedByGear(char[][] matrix, int row, int col, int size) {
+        if (row != 0 && col != 0 && row != size - 1 && col != size - 1) {
+            if (matrix[row + 1][col + 1] == '*') return (row + 1) + "," + (col + 1);
+            else if (matrix[row - 1][col - 1] == '*') return (row - 1) + "," + (col - 1);
+            else if (matrix[row - 1][col + 1] == '*') return (row - 1) + "," + (col + 1);
+            else if (matrix[row + 1][col - 1] == '*') return (row + 1) + "," + (col - 1);
+            else if (matrix[row][col - 1] == '*') return row + "," + (col - 1);
+            else if (matrix[row][col + 1] == '*') return row + "," + (col + 1);
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+        } else if (row == 0 && col == 0) {
+            if (matrix[row + 1][col + 1] == '*') return (row + 1) + "," + (col + 1);
+            else if (matrix[row][col + 1] == '*') return row + "," + (col + 1);
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+        } else if (row == 0 && col == size - 1) {
+            if (matrix[row + 1][col - 1] == '*') return (row + 1) + "," + (col - 1);
+            else if (matrix[row][col - 1] == '*') return row + "," + (col - 1);
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+        } else if (row == size - 1 && col == 0) {
+            if (matrix[row - 1][col + 1] == '*') return (row - 1) + "," + (col + 1);
+            else if (matrix[row][col + 1] == '*') return row + "," + (col + 1);
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+        } else if (row == size - 1 && col == size - 1) {
+            if (matrix[row - 1][col - 1] == '*') return (row - 1) + "," + (col - 1);
+            else if (matrix[row][col - 1] == '*') return row + "," + (col - 1);
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+        } else if (row == 0 && col != size - 1) {
+            if (matrix[row][col + 1] == '*') return row + "," + (col + 1);
+            else if (matrix[row][col - 1] == '*') return row + "," + (col - 1);
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+            else if (matrix[row + 1][col + 1] == '*') return (row + 1) + "," + (col + 1);
+            else if (matrix[row + 1][col - 1] == '*') return (row + 1) + "," + col;
+        } else if (col == 0) {
+            if (matrix[row][col + 1] == '*') return row + "," + col;
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+            else if (matrix[row + 1][col + 1] == '*') return (row + 1) + "," + (col + 1);
+            else if (matrix[row - 1][col + 1] == '*') return (row - 1) + "," + (col + 1);
+        } else if (row == size - 1) {
+            if (matrix[row][col + 1] == '*') return row + "," + col;
+            else if (matrix[row][col - 1] == '*') return row + "," + (col - 1);
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+            else if (matrix[row - 1][col + 1] == '*') return (row - 1) + "," + (col + 1);
+            else if (matrix[row - 1][col - 1] == '*') return (row - 1) + "," + (col - 1);
+        } else if (col == size - 1 && row != 0) {
+            if (matrix[row][col - 1] == '*') return row + "," + col;
+            else if (matrix[row + 1][col - 1] == '*') return (row + 1) + "," + (col - 1);
+            else if (matrix[row - 1][col - 1] == '*') return (row - 1) + "," + (col - 1);
+            else if (matrix[row - 1][col] == '*') return (row - 1) + "," + col;
+            else if (matrix[row + 1][col] == '*') return (row + 1) + "," + col;
+        }
+
+        return null;
+    }
+
 
 }
