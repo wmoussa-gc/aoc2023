ab33c508a2d9cc6c648edd83dc9324982b71d309
Wissam Moussa <wissam.moussa@cds-snc.ca>
2023-12-02 21:10:22 -0500

    day2

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -1,4 +1,4 @@
-package day2;
+package day3;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,32 +7,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //part1();
-        part2();
+        part1();
+        //part2();
     }
 
     private static void part1() throws IOException {
-        String fileName = "src/day2/input.txt";
+        String fileName = "src/day3/sample.txt";
         List<String> lines = Files.readAllLines(Path.of(fileName));
-        int sum = 0;
+
         for (String line : lines) {
-            int gameId = Integer.parseInt(line.split(":")[0].split(" ")[1]);
-            String[] reveals = line.split(":")[1].split(";");
-            boolean is_possible = true;
-            for (String reveal : reveals) {
-                String[] balls = reveal.split(",");
-                for (String ball : balls) {
-                    String[] temp = ball.trim().split(" ");
-                    int x = Integer.parseInt(temp[0]);
-                    String color = temp[1];
-                    if (color.equals("red") && x > 12) is_possible = false;
-                    if (color.equals("green") && x > 13) is_possible = false;
-                    if (color.equals("blue") && x > 14) is_possible = false;
-                }
-            }
-            if (is_possible) sum += gameId;
+
         }
-        System.out.println(sum);
     }
 
     private static void part2() throws IOException {
